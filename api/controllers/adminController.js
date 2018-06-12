cade50e5ca2a2ce6f7d4980c264ab07ad49ad412
lakmal <lada@mazarin.lk>
2018-06-12 22:22:46 +0800

    first commit

---
+++ api/controllers/adminController.js
@@ -0,0 +1,18 @@
+exports.list_all_tasks = function (req, res) {
+  res.json({id: '1', name: 'test'})
+};
+
+
+exports.create_a_task = function (req, res) {
+
+};
+
+
+exports.read_a_task = function (req, res) {
+
+};
+
+
+exports.update_a_task = function (req, res) {
+
+};
